162f8a3e6a8f2d89c6fd0aa86768270e95b263ce
VONETTA <thevonetteway@gmail.com>
2022-06-06 15:25:15 +0300

    added changes to navbar,base.html,views.py

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from instagram.forms import SignupForm
+from .models import *
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+# from .tokens import account_activation_token
+from django.utils.encoding import force_bytes
+
+
+
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            current_user = form.save(commit=False)
+            current_user.is_active = False
+            current_user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your instapicha account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': current_user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(current_user.pk)),
+                # 'token':account_activation_token.make_token(current_user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
