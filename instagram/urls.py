09cbd23152b3c95fb54ae491adda6e53f3a2f1cb
VONETTA <thevonetteway@gmail.com>
2022-06-06 20:56:36 +0300

    added imports to urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from.import views
+from instagram import views
 
 urlpatterns = [
      # path('',views.home,name = 'home'),
-     path('',views.signup,name='signup')
+     path('',views.signup,name='signup'),
 ]
